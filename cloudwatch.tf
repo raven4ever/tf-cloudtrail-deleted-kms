ceade1ad871b0c2cfeb952f69d199567aed9b77b
raven4ever <wrtv23@gmail.com>
2023-01-20 13:17:04 +0200

    wrong metric name

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -18,7 +18,7 @@ resource "aws_cloudwatch_log_metric_filter" "kms_use_deleted_key_filter" {
 resource "aws_cloudwatch_metric_alarm" "kms_use_deleted_key_alarm" {
   alarm_name          = var.cw_alarm_name
   alarm_description   = "This metric monitors usage of pending deletion KMS keys"
-  metric_name         = aws_cloudwatch_log_metric_filter.kms_use_deleted_key_filter.name
+  metric_name         = "KMSKeyPendingDeletionErrorCount"
   namespace           = "CloudTrailLogMetrics"
   datapoints_to_alarm = 1
   treat_missing_data  = "notBreaching"
